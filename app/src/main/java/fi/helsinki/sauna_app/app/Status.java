5c01b53c580f1716d0f4edd7d7c8b2995bdb2a17
Jeuro <juuuro@gmail.com>
2014-04-14 20:40:44 +0300

    add sensordrone library

--- app/src/main/java/fi/helsinki/sauna_app/app/Status.java
+++ app/src/main/java/fi/helsinki/sauna_app/app/Status.java
@@ -4,6 +4,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.sensorcon.sensordrone.android.Drone;
 
 public class Status extends ActionBarActivity {
 
@@ -13,10 +17,8 @@ public class Status extends ActionBarActivity {
         setContentView(R.layout.activity_status);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.status, menu);
         return true;
@@ -34,4 +36,21 @@ public class Status extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void measure(View view) {
+        Drone d = new Drone();
+        d.btConnect("00:17:E9:50:E1:75");
+
+        d.enableTemperature();
+        d.measureTemperature();
+        TextView tempValue = (TextView) findViewById(R.id.temperature);
+        tempValue.setText("Temperature: " + d.temperature_Celsius + " °C");
+
+        d.enableHumidity();
+        d.measureHumidity();
+
+        TextView humidValue = (TextView) findViewById(R.id.humidity);
+        humidValue.setText("Humidity: " + d.humidity_Percent + " %");
+
+        d.disconnect();
+    }
 }
